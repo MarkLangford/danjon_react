8ff900740745119606dfc2d11ff25652611cdd09
Mark Langford <marklangford92@gmail.com>
2022-06-21 21:25:55 +0100

    Added Lore content

--- src/pages/Lore.js
+++ src/pages/Lore.js
@@ -4,7 +4,11 @@ import text from "./LoreText.json";
 export default function Lore() {
   return (
     <div className="Lore_Text">
+      <h3>Part 1</h3>
       <p>{text[0].part1}</p>
+      <br />
+      <h3>Part 2</h3>
+      <p>{text[1].part2}</p>
     </div>
   );
 }
